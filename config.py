379740b7bf58ff0016a1c46c1896d68b0113ebab
Jake <jake.adams92@gmail.com>
2014-05-20 16:44:10 +0930

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+config = {"host":"TS-Server.com",
+          "port":"10011",
+          "user":"USERNAME",
+          "pass":"PASSWORD",
+          "idle": "10",#in minutes
+          "botname":"Idlebot",
+          "serverid":"1"
+          }
